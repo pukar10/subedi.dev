c4f488df119b8faf5172972713467e4d66127369
Pukar Subedi <subedipukar@gmail.com>
2024-07-01 14:26:33 -0400

    added a top menu and collapasable sidebar, logo and phone number on top menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-// js/index.js
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('menu-toggle');
-    const body = document.body;
-
-    toggleButton.addEventListener('click', function() {
-        body.classList.toggle('menu-open');
+    const menuButton = document.querySelector('.menu-button');
+    const closeButton = document.querySelector('.close-button');
+    const sideMenu = document.querySelector('.side-menu');
+  
+    menuButton.addEventListener('click', function() {
+      sideMenu.style.left = '0'; // Show the menu
     });
-});
+  
+    closeButton.addEventListener('click', function() {
+      sideMenu.style.left = '-100%'; // Hide the menu
+    });
+  });
